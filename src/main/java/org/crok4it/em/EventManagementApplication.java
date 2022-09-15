e3d07e6f98ecbced90eaf269cc62dfa08e13e2ed
ambiandji <ambiandji@gmail.com>
2022-09-15 18:27:12 +0100

    feat: adding swagged ui documenttation #21

--- src/main/java/org/crok4it/em/EventManagementApplication.java
+++ src/main/java/org/crok4it/em/EventManagementApplication.java
@@ -1,9 +1,15 @@
 package org.crok4it.em;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+        title = "Event Management Application",
+        version = "1.0",
+        description = "Manage Events"))
 public class EventManagementApplication {
 
     public static void main(String[] args) {
--- src/main/java/org/crok4it/em/config/SwaggerConfig.java
+++
@@ -1,4 +0,0 @@
-package org.crok4it.em.config;
-
-public class SwaggerConfig {
-}
