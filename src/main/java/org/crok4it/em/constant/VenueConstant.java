17f2b15b9038437c3e4d8079c63719ce06eb7d18
ambiandji <ambiandji@gmail.com>
2022-09-15 23:11:14 +0100

    feat: updating openapi specs

--- src/main/java/org/crok4it/em/constant/VenueConstant.java
+++ src/main/java/org/crok4it/em/constant/VenueConstant.java
@@ -3,5 +3,5 @@ package org.crok4it.em.constant;
 import static org.crok4it.em.constant.CommonConstant.API_BASE_ROUTE;
 
 public class VenueConstant {
-    public static final String API_VENUE_BASE_ROUTE = API_BASE_ROUTE + "/venue";
+    public static final String API_VENUE_BASE_ROUTE = API_BASE_ROUTE + "/venues";
 }
